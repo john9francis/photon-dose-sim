66766eb4a4793ea87e557477c0eb865b4aa26d7e
John Francis <john9francis@gmail.com>
2023-07-05 11:56:04 -0400

    most basic Geant4 project

---
+++ photon-dose-sim.cpp
@@ -0,0 +1,13 @@
+﻿// photon-dose-sim.cpp : Defines the entry point for the application.
+//
+
+#include "photon-dose-sim.h"
+#include "G4UIExecutive.hh"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	G4UIExecutive ui(argc, argv);
+	ui.SessionStart();
+}
