279b03e245228f7befcc5507711d8902649d7fa3
John Francis <john9francis@gmail.com>
2023-07-12 17:56:11 -0400

    created primary generator action class

---
+++ include/PrimaryGeneratorAction.hh
@@ -0,0 +1,14 @@
+#ifndef PhotonDoseSimPrimaryGeneratorAction_h
+#define PhotonDoseSimPrimaryGeneratorAction_h 1
+
+// defines the photon particle gun
+// goal: shoot 100 keV energy photons. 
+
+
+
+
+
+
+
+
+#endif
